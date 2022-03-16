2cfd38913d607b0bd8383611cde5cd6325fbbb46
ethan-r-curtis <89948721+ethan-r-curtis@users.noreply.github.com>
2022-03-16 14:07:49 -0700

    Fix missing json (#13)
    * Fixed bug with graphing after restart, hopefully
    
    * Fix return error when batch size reaches 1
    
    * Updated information being printed after each epoch
    
    * re-added DIE if job fails twice
    
    * :
    
    * Rewrote reloading of validation performance
    
    * )
    
    * Fixed redoing failed calcs
    
    * Added tests, fixed old ones

---
+++ tests/test_tccloudengine.py
@@ -0,0 +1,56 @@
+from ff_optimizer import qmengine
+import os
+from qcelemental.util.serialization import json_loads
+from tccloud.models import AtomicResult
+
+def test_init():
+    os.chdir(os.path.dirname(__file__))
+    tccloudEngine = qmengine.TCCloudEngine("qmengine/tc.in","qmengine/tc_backup.in")
+    assert tccloudEngine.method == "b3lyp"
+    assert tccloudEngine.basis == "6-31gss"
+    assert tccloudEngine.keywords['dftd'] == 'd3'
+    assert tccloudEngine.keywords['charge'] == '0' 
+    assert tccloudEngine.backupKeywords['threall'] == '1.0e-14'
+    assert tccloudEngine.backupKeywords['diismaxvecs'] == '40'
+    assert tccloudEngine.backupKeywords['maxit'] == '200'
+
+def testGetQMRefData(monkeypatch):
+    os.chdir(os.path.dirname(__file__))
+    tccloudEngine = qmengine.TCCloudEngine("qmengine/tc.in","qmengine/tc_backup.in")
+    calcDir = "tccloudengine"
+    pdbs = []
+    jsons = []
+    for f in os.listdir(calcDir):
+        if f.endswith("pdb"):
+            pdbs.append(f)
+        if f.endswith("json"):
+            jsons.append(f)
+    results = []
+    for json in jsons:
+        with open(os.path.join(calcDir,json),'r') as f:
+            results.append(AtomicResult(**json_loads(f.read())))
+
+    def monkeyCompute(atomicInputs):
+        if len(atomicInputs) == 10:
+            return 0, results
+        else:
+            ids = sorted([input.id for input in atomicInputs])
+            refIds = ['3','6','9']
+            assert len(atomicInputs) == 3
+            assert atomicInputs[0].keywords['threall'] == '1.0e-14'
+            assert atomicInputs[0].keywords['diismaxvecs'] == '40'
+            for i in range(len(ids)):
+                assert ids[i] == refIds[i]
+            return 0, results[:2]
+
+    def monkeyRead(*args):
+        return [], [], [], [], []
+
+    def monkeyWrite(*args):
+        pass
+
+    monkeypatch.setattr(tccloudEngine,"computeBatch",monkeyCompute)
+    monkeypatch.setattr(tccloudEngine,"writeResult",monkeyWrite)
+    monkeypatch.setattr(qmengine.QMEngine,"readQMRefData",monkeyRead)
+    monkeypatch.setattr(qmengine.QMEngine,"writeFBdata",monkeyWrite)
+    tccloudEngine.getQMRefData(pdbs,calcDir)
