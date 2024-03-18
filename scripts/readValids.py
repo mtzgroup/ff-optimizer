08b692de2e03980afb03e84f51871dd6f804125b
Ethan Curtis <curtie@stanford.edu>
2024-03-18 16:55:03 -0700

    Added edits to readValids script for determining convergence

--- scripts/readValids.py
+++ scripts/readValids.py
@@ -65,15 +65,16 @@ def runValid(optdir, inp, out, parmFolder, frcmod, mol2):
     home = os.getcwd()
     optdir = Path(optdir)
     parmFolder = Path(parmFolder)
-    copyfile(parmFolder / frcmod, optdir / "targets" / frcmod)
-    copyfile(parmFolder / mol2, optdir / "targets" / mol2)
+    copyfile(parmFolder / frcmod, optdir / "forcefield" / frcmod)
+    copyfile(parmFolder / mol2, optdir / "forcefield" / mol2)
     os.chdir(optdir)
     os.system(f"ForceBalance.py {inp} > {out}")
     v = readValid(out)
     os.chdir(home)
     return v
 
-def getFinalValidations(i, lastCycle, optdir="1_opt", patience=5):
+def getFinalValidations(i, lastCycle, optdir="1_opt", patience=5, frcmod="dasa.frcmod", mol2="dasa.mol2"):
+    optdir = Path(optdir)
     vs = []
     # only check the parameters in the patience region
     for j in range(lastCycle-patience, lastCycle + 1):
@@ -83,12 +84,13 @@ def getFinalValidations(i, lastCycle, optdir="1_opt", patience=5):
             inp = f"valid_{lastCycle}_{i}.in"
         out = f"valid_{j}_{i}_final.out"
         try:
-            vs.append(readValid(out))
+            vs.append(readValid(optdir / out))
         except:
-            runValid(optdir, inp, out, f"{optdir}/result/opt_{j}", "dasa.frcmod", "dasa.mol2")
-            vs.append(readValid(Path(optdir) / out))
+            runValid(optdir, inp, out, f"{optdir}/result/opt_{j}", frcmod, mol2)
+            vs.append(readValid(optdir / out))
     vs = np.asarray(vs)
-    return np.argmin(vs) + 1 # account for one-indexing of validations
+    print(vs)
+    return np.argmin(vs) + 1 + lastCycle - patience # account for one-indexing of validations
         
 def readValidDiff(optdir="1_opt"):
     valid = readValids("", optdir)
@@ -101,7 +103,6 @@ def readValidDiff(optdir="1_opt"):
 vj = readValidDiff()
 for i in range(vj.shape[1]):
     finalCycle = getStoppingPoint(vj[:,i])
-    print(finalCycle)
     best = getFinalValidations(i, finalCycle)
     print(best)
 #np.savetxt("validPrev.txt", validPrev, fmt="%10.3e")
