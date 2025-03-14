b1e03216438b81d8e9b1d2ff4d9b7fcc5bfc4688
ethan-r-curtis <89948721+ethan-r-curtis@users.noreply.github.com>
2025-03-14 16:14:14 -0700

    Added readmes and comments for examples (#113)

--- examples/demo/2_sampling/sbatch_template.sh
+++ examples/demo/2_sampling/sbatch_template.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+# Slurm parameters, you may need to adapt these for your cluster
+
 #SBATCH -t 1:00:00
 #SBATCH -J FB_ref_gradient_JOBID
 #SBATCH --gres gpu:1
 #SBATCH -p gpu_shortq
 #SBATCH --qos=gpu_short
 #SBATCH --mem=8GB
+
+# To write the sbatch files used by ff-opt, it replaces certain
+# all-caps variables with the appropriate value: 
+# JOBID - the ID of the xyz file used for a gradient calculation
+# TCTEMPLATE - name of the terachem template input
+# TCTEMPLATEBACKUP - name of the backup terachem template input
+
+# The Martinez lab cluster uses a scratch folder system where
+# $SCRATCH is set in the environment. You can remove this if
+# your cluster doesn't do something similar. 
 cp JOBID.xyz TCTEMPLATE TCTEMPLATEBACKUP $SCRATCH/.
 cd $SCRATCH
 
-ml TeraChem
+# Load the appropriate terachem module
+module load TeraChem
 terachem TCTEMPLATE > tc_JOBID.out
 echo JOBID
+
+# Re-run using backup tc input if the job failed
 if [ $(grep -c "Job finished" tc_JOBID.out) -ne 1 ]
 then
   terachem TCTEMPLATEBACKUP > tc_JOBID.out
